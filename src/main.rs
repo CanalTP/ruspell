cc2b72852c477cade7dcc822c0b2a2c9add82e0a
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2017-03-09 16:11:49 +0100

    adds some fixed-case words, new regex, all snake-case

--- src/main.rs
+++ src/main.rs
@@ -159,8 +159,8 @@ fn first_upper_all_lower(name: String) -> String {
 }
 
 
-/// specific management of full-uppercase names
-fn process_full_upper(name: String) -> String {
+/// every word becomes Mmmmmm
+fn snake_case(name: String) -> String {
     let mut new_name = String::new();
     for word in get_words(&name) {
         new_name.push_str(&first_upper_all_lower(word.to_string()));
@@ -169,23 +169,28 @@ fn process_full_upper(name: String) -> String {
 }
 
 
+use regex::Regex;
+use regex::RegexSet;
 fn must_be_lower(text: &str) -> bool {
     lazy_static! {
         static ref RE: Regex =
-            Regex::new(r"(?i)^(en|sur|sous|de|du|des|le|la|les|au|aux|un|une|à)$").unwrap();
+            Regex::new(r"(?i)^(en|sur|et|sous|de|du|des|le|la|les|au|aux|un|une|à)$").unwrap();
     }
     RE.is_match(text)
 }
 
 fn must_be_upper(text: &str) -> bool {
     lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"(?i)^(RER|EDF|INRIA|CRC|HEC|SNCF|RATP|HLM|ZA|ZI|CCI|RN\d*|\w*\d\w*|RD\d*|(XL|X{0,3})(IX|IV|V?I{0,3}))$").unwrap();
+        static ref RE: RegexSet =
+            RegexSet::new(&[
+                r"(?i)^(RER|CDG|CES|ASPTT|PTT|EDF|INRIA|INRA|CRC|HEC|SNCF|RATP|HLM|CHR|CHU)$",
+                r"(?i)^(ZA|ZI|RPA|CFA|CC|CCI|UFR|CPAM|ANPE|RN\d*|\w*\d\w*|RD\d*)$",
+                r"(?i)^(XL|X{0,3})(IX|IV|V?I{0,3})$",
+                ]).unwrap();
     }
     RE.is_match(text)
 }
 
-use regex::Regex;
 fn sed_all(name: String) -> String {
     if must_be_lower(&name) {
         return name.to_lowercase();
@@ -212,9 +217,37 @@ fn regex_all_name(name: String) -> String {
         static ref RE_SAINT: Regex =
             Regex::new(r"(?i)(^|\W)s(?:ain)?t(e?)\W").unwrap();
     }
-    let res = RE_SAINT.replace_all(&name, |caps: &Captures| {
-        format!("{}Saint{}-", &caps[1], &caps[2].to_lowercase())
-    });
+    let res = RE_SAINT.replace_all(&name, "${1}Saint${2}-");
+
+    lazy_static! {
+        static ref RE_ND: Regex =
+            Regex::new(r"(?i)(^|\W)n(?:otre)?[ -]*d(?:ame)?(\W|$)").unwrap();
+    }
+    let res = RE_ND.replace_all(&res, "${1}Notre-Dame${2}");
+
+    lazy_static! {
+        static ref RE_PLACE: Regex =
+            Regex::new(r"(?i)(^|\W)pl\.?(\W|$)").unwrap();
+    }
+    let res = RE_PLACE.replace_all(&res, "${1}Place${2}");
+
+    lazy_static! {
+        static ref RE_BOULEVARD: Regex =
+            Regex::new(r"(?i)(^|\W)bl?v?d\.?(\W|$)").unwrap();
+    }
+    let res = RE_BOULEVARD.replace_all(&res, "${1}Boulevard${2}");
+
+    lazy_static! {
+        static ref RE_LIEU_DIT: Regex =
+            Regex::new(r"(?i)(^|\W)rte(\W|$)").unwrap();
+    }
+    let res = RE_LIEU_DIT.replace_all(&res, "${1}Route${2}");
+
+    lazy_static! {
+        static ref RE_SAINT_LOUIS: Regex =
+            Regex::new(r"(?i)(^|\W)Saint-Louis(\W|$)").unwrap();
+    }
+    let res = RE_SAINT_LOUIS.replace_all(&res, "${1}Saint Louis${2}");
 
     lazy_static! {
         static ref RE_A: Regex =
@@ -223,12 +256,25 @@ fn regex_all_name(name: String) -> String {
     let res = RE_A.replace_all(&res, " à ");
 
     lazy_static! {
+        static ref RE_ROND_POINT: Regex =
+            Regex::new(r"(?i)(^|\W)ro?n?d[ \.-]?po?i?n?t ").unwrap();
+    }
+    let res = RE_ROND_POINT.replace_all(&res, "${1}Rond-Point ");
+
+    lazy_static! {
+        static ref RE_SPACES: Regex =
+            Regex::new(r"(?i)  +").unwrap();
+    }
+    let res = RE_SPACES.replace_all(&res, " ");
+
+    lazy_static! {
         static ref RE_QUOTE: Regex =
-            Regex::new(r"(?i)(^|\W)([ld])[ '](h?[aeiouy])").unwrap();
+            Regex::new(r"(?i)(^|\W)([ld])[ '](h[aiouye]|[aiouy]|et[^ ]|e[^t].)").unwrap();
     }
     let res = RE_QUOTE.replace_all(&res, |caps: &Captures| {
         format!("{}{}'{}", &caps[1], &caps[2].to_lowercase(), &caps[3])
     });
+
     res.into_owned()
 }
 
@@ -236,17 +282,16 @@ fn regex_all_name(name: String) -> String {
 /// management of all names
 fn process_record(rec: &Record) -> Option<RecordRule> {
     let mut new_name = decode(rec.name.clone());
-    if new_name.chars().all(|c| !c.is_lowercase()) {
-        new_name = process_full_upper(new_name);
-    }
+    new_name = snake_case(new_name);
+
     let mut tmp = String::new();
     for word in get_words(&new_name) {
         tmp.push_str(&sed_all(word.to_string()));
     }
+
     new_name = regex_all_name(tmp);
     new_name = first_upper(new_name);
 
-    //println!("{} >> {:?}", rec.name, get_words(&new_name));
     if rec.name == new_name {
         None
     } else {
